63d12ffc8203e4af0694a59eb70421656cf83f60
Hojun Ryu <ho7221@gmail.com>
2023-08-01 14:32:53 +0900

    Initial Commit

---
+++ src/main/kotlin/cc/connectcampus/connect_campus/ConnectCampusApplication.kt
@@ -0,0 +1,11 @@
+package cc.connectcampus.connect_campus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConnectCampusApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConnectCampusApplication>(*args)
+}
---
+++ src/test/kotlin/cc/connectcampus/connect_campus/ConnectCampusApplicationTests.kt
@@ -0,0 +1,13 @@
+package cc.connectcampus.connect_campus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConnectCampusApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

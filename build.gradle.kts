617a244f8d9538e77850c67515b8e0b6f98c3ef7
moon <88218891+man-moon@users.noreply.github.com>
2023-08-07 19:08:37 +0900

    [새로운 기능] Querydsl 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 
 plugins {
     id("org.springframework.boot") version "3.1.2"
@@ -6,6 +7,7 @@ plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
     kotlin("plugin.jpa") version "1.8.22"
+    kotlin("kapt") version "1.8.22"
 }
 
 group = "cc.connectcampus"
@@ -29,6 +31,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     compileOnly("org.projectlombok:lombok")
@@ -43,9 +46,10 @@ dependencies {
 
     //querydsl
     implementation ("com.querydsl:querydsl-jpa:5.0.0:jakarta")
-    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jpa")
-    annotationProcessor ("jakarta.annotation:jakarta.annotation-api")
-    annotationProcessor ("jakarta.persistence:jakarta.persistence-api")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+//    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jpa")
+//    annotationProcessor ("jakarta.annotation:jakarta.annotation-api")
+//    annotationProcessor ("jakarta.persistence:jakarta.persistence-api")
 
     //spring security
 //    implementation("org.springframework.boot:spring-boot-starter-security")
